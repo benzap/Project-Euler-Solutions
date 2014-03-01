ae829a841a86abfbff90086c4c2feae86fbf1a8d
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2014-03-01 15:01:15 -0500

    Made it even faster, problem 3 solved

--- problem-003/src/problem_003/core.clj
+++ problem-003/src/problem_003/core.clj
@@ -8,8 +8,7 @@
 
 (defn isprime? [num]
   (cond
-   (< num 2) false
-   (= num 2) true
+   (<= num 2) false
    (= num 3) true
    (zero? (mod num 2)) false
    (zero? (mod num 3)) false
@@ -23,13 +22,17 @@
 (isprime? 29)
 
 (defn prime-factors [num]
-  (let [seq-primes (filter #(isprime? %) (range num))]
+  (let [max-divisor (int (math/ceil (math/sqrt num)))
+        seq-primes (filter #(isprime? %) (range max-divisor))]
     (filter #(zero? (mod num %)) seq-primes)))
 
 (apply max (prime-factors 13195))
 
 (defn soln []
   (apply max (prime-factors 600851475143)))
+
+(prime-factors 100000000)
+
  
 (defn -main
   "Solution to problem 3"
