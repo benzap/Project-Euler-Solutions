d720e9a164813dfdf13db52e4fe8239d969d3609
Benjamin Zaporzan <benzaporzan@gmail.com>
2014-03-01 16:50:48 -0500

    Finished Problem 4

--- problem-004/project.clj
+++ problem-004/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot problem-004.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- problem-004/src/problem_004/core.clj
+++ problem-004/src/problem_004/core.clj
@@ -1,7 +1,28 @@
 (ns problem-004.core
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.string :as s])
   (:gen-class))
 
+(defn ispalindrome? [num]
+  (let [num-str (str num)
+        num-length (count num-str)
+        num-slice-depth (int (/ num-length 2))
+        str-left (subs num-str 0 num-slice-depth)
+        str-right (subs num-str (- num-length num-slice-depth))]
+    (= str-left (s/reverse str-right))))
+
+;;(ispalindrome? 1100000011)
+
+(defn nth-digit-palindrome-list [num]
+  (let [lower-num (math/expt 10 (dec num))
+        upper-num (math/expt 10 num)
+        num-range (range lower-num upper-num)
+        mult-list (for [i num-range j num-range] (* i j))]
+    (filter ispalindrome? mult-list)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Started Problem 4 Calculation...")
+  (print " : ")
+  (println (apply max (nth-digit-palindrome-list 3))))
+  
